99d9e4f012e22e642a50b32951b341b38ac5540f
Mayank <mayank2002singh@gmail.com>
2025-01-05 01:50:26 +0530

    cookie changes

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(
       secure: process.env.NODE_ENV === 'production',
       httpOnly: true,
       maxAge: 1000 * 60 * 60 * 24,  // 1 day
-      sameSite: 'lax',  // Prevents CSRF attacks
+      sameSite: 'none',  
     },
   })
 );
